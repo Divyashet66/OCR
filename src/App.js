5ad9e9cc91b56bfc7572ac3401815081c833c9b2
Divya Shet <88928587+Divyashet66@users.noreply.github.com>
2022-09-30 00:24:47 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import { useCallback, useEffect, useState } from 'react';
+import { createWorker } from 'tesseract.js';
+import './App.css';
+import styled, { keyframes } from "styled-components"
+
+const animation = keyframes`
+  0% { opacity: 0; transform: translateY(-100px); }
+  25% { opacity: 1; transform: translateY(0px); }
+  75% { opacity: 1; transform: translateY(0px); }
+  100% { opacity: 0; transform: translateY(-100px); }
+`
+const Wrapper = styled.span`
+  animation-name: ${animation};
+  animation-duration: 6s;
+  animation-fill-mode: forwards;
+  animation-iteration-count: infinite;
+  font-size: 50px;
+  font-weight: 600;
+`
+
+
+function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [textResult, setTextResult] = useState("");
+
+  const worker = createWorker();
+
+  const convertImageToText = useCallback(async () => {
+    if(!selectedImage) return;
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const { data } = await worker.recognize(selectedImage);
+    setTextResult(data.text);
+  }, [worker, selectedImage]);
+
+  useEffect(() => {
+    convertImageToText();
+  }, [selectedImage, convertImageToText])
+
+  const handleChangeImage = e => {
+    if(e.target.files[0]) {
+      setSelectedImage(e.target.files[0]);
+    } else {
+      setSelectedImage(null);
+      setTextResult("")
+    }
+  }
+
+  return (
+    <div className="App">
+      <Wrapper>OCR App</Wrapper>
+      <p className='waviy'>Extract Text Anytime, Anywhere!</p>
+      <div className="input-wrapper">
+        <label htmlFor="upload">Upload Image</label>
+        <input type="file" id="upload" accept='image/*' onChange={handleChangeImage} />
+      </div>
+
+      <div className="result">
+        {selectedImage && (
+          <div className="box-image">
+            <img src={URL.createObjectURL(selectedImage)} alt="thumb" />
+          </div>
+        )}
+        {textResult && (
+          <div className="box-p">
+            <p>{textResult}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
